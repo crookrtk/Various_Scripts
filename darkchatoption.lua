a49faea5183b110622879c7a4ea95c67738a7e87
crookrtk <82241891+crookrtk@users.noreply.github.com>
2022-09-18 16:32:06 -0700

    Create darkchatoption.lua

---
+++ darkchatoption.lua
@@ -0,0 +1,13 @@
+	--- dark chat loader
+local Players = game:GetService("Players")
+local Player = Players.LocalPlayer
+local PlayerGui = Player:WaitForChild("PlayerGui")
+local BubbleChatUI = PlayerGui:WaitForChild("BubbleChat")
+BubbleChatUI.DescendantAdded:Connect(function(Descendant)
+if Descendant:IsA("ImageLabel") then
+Descendant.ImageColor3 = Color3.fromRGB(59, 59, 59)
+elseif Descendant:IsA("TextLabel") then
+Descendant.TextColor3 = Color3.new(1,1,1)
+end
+end)
+end
